598661f61c127813f77974dd1e15cf33f2802a0a
Nadine98 <Nadine.Kais@ruhr-uni-bochum.de>
2024-02-08 09:40:47 +0100

    [Experiment_1] Changing the experiment 1

--- thesis.py
+++ thesis.py
@@ -1,13 +1,15 @@
 from neo4j import GraphDatabase
 import pandas as pd
+import numpy as np
 from time import time as timer 
 
 
 class experiments: 
     def __init__(self):
 
-        self.databases = ["grundriss2", "grundriss2RDF"]
+        self.databases = ["grundriss2", "grundriss2RDF", "grundriss6", "grundriss6RDF", "grundriss8", "grundriss8RDF", "grundriss10", "grundriss10RDF"]
         
+        # For Connection to the Neo4j Database Management Sytsem
         self.__URI = "bolt://localhost:7687"
         self.__AUTH = ("neo4j", "kais1234")
 
@@ -53,44 +55,68 @@ class experiments:
     def setup(self, tx, dbType, nameDB):
        
        # Query of type 1
-       measure_type1 = self.executeQueries(tx,self.queries[self.queries['Type']==1][dbType].to_list())
-
+       measure_type1 = self.executeQueries(tx,self.queries[self.queries['Type']==1][dbType].to_list(), 1)
        # Query of type 2
-       measure_type2 = self.executeQueries(tx, self.queries[self.queries['Type']==2][dbType].to_list())
-
+       measure_type2 = self.executeQueries(tx, self.queries[self.queries['Type']==2][dbType].to_list(), 2)
        # Query of type 3
-       measure_type3 = self.executeQueries(tx, self.queries[self.queries['Type']==3][dbType].to_list())
+       measure_type3 = self.executeQueries(tx, self.queries[self.queries['Type']==3][dbType].to_list(), 3)
 
-       # Safing the result of a graph in this dict
-       self.graphs_measures[nameDB]=[measure_type1, measure_type2, measure_type3]
-       print(nameDB)
-       print(self.graphs_measures[nameDB][0])
+       # Safe results of each graph in this dict 
+       self.graphs_measures[nameDB]=pd.concat([measure_type1.T, measure_type2.T, measure_type3.T])
 
-                
-
+     
+    
+      
     # Execute the queries of a certain type and for a certain number of iteration
-    def executeQueries(self, tx, set): 
-        measuresType = dict({"query1":[], "query2":[], "query3":[], "query4":[]})
-        
-        for _ in range(self.numberOfIteration):
-            for i , query in enumerate(set): 
+    def executeQueries(self, tx, set, typeNumber): 
+        measuresType = pd.DataFrame({
+                            f"Type{typeNumber}_query1":{"Type":None, "query":None, "Mean":None, "Std":None, "Median":None}, 
+                            f"Type{typeNumber}_query2":{"Type":None, "query":None, "Mean":None, "Std":None, "Median":None},
+                            f"Type{typeNumber}_query3":{"Type":None, "query":None, "Mean":None, "Std":None, "Median":None},
+                            f"Type{typeNumber}_query4":{"Type":None, "query":None, "Mean":None, "Std":None, "Median":None}
+                        })
 
+        
+        for i , query in enumerate(set):
+
+            # Saving the measured time
+            execution_time = []
+
+            for _ in range(self.numberOfIteration):
                 start = timer()
                 result= tx.run(query)
                 end = timer()
 
-             
-                # Saving the time in mi
-                measuresType[f"query{i+1}"].append( (end-start) *1e6)
+                # Saving the time in microsecond
+                key = f"Type{typeNumber}_query{i+1}"
+                execution_time.append( (end-start) *1e6)
+
+            # Calculate the measures and save them
+            measuresType[key]["Type"]=typeNumber
+            measuresType[key]["query"] = f"query_{i +1}"
+            measuresType[key]["Mean"] = np.mean(execution_time)
+            measuresType[key]["Median"] = np.median(execution_time)
+            measuresType[key]["Std"] = np.std(execution_time)
+
+
         return measuresType
 
     # Get the queries for the file
     def getQueriesFromFile(self,file):
         return pd.read_excel(file)
+    
+    # Create for every graph a cvs file
+    def convertResults(self): 
+        for db in self.databases:
+            df = self.graphs_measures[db]
+            df.to_csv(f"./experiment_1/{db}_measures", index=False)
+            
+
 
 if __name__ == "__main__":
-    o = experiments(1000)
-    o.experiment("./experiment_1.xlsx")
+    o = experiments()
+    o.experiment("./experiment_1.xlsx",10)
+    o.convertResults()
 
 
 
