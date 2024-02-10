7e23a34b252e7abb7a3bf42581664d051878300a
Nadine98 <Nadine.Kais@ruhr-uni-bochum.de>
2024-02-10 22:50:50 +0100

    [Experiments] Changing the Experiment 1 and adding a new Experiment

--- experiments.py
+++ experiments.py
@@ -2,35 +2,31 @@ from neo4j import GraphDatabase
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
-from time import time as timer 
-import psutil
-import os
+import time  
 
 
 
 class performanceTest: 
     def __init__(self, file, numberOfIteration):
 
+        # Graphs that are saved in neo4j
         self.databases = ["grundriss2", "grundriss2RDF", "grundriss6", "grundriss6RDF", "grundriss8", "grundriss8RDF", "grundriss10", "grundriss10RDF"]
-        
-        # For connecting to the Neo4j Database Management Sytsem
-        self.__URI = "bolt://localhost:7687"
-        self.__AUTH = ("neo4j", "kais1234")
 
         # Adding the execution times for graph (LPG and RDF graph)
-        self.graphs_measures={}
-    
+        self.graphs_metrics={}
+
         # Number for running each query again -> to get a stable result
         self.numberOfIteration = numberOfIteration
+
+        # File where the queries are stored
         self.file = file 
 
 
-
     def executeExperiment(self):
-    
-        self.numberOfIteration = self.numberOfIteration
+        URI = "bolt://localhost:7687"
+        AUTH = ("neo4j", "kais1234")
 
-        with  GraphDatabase.driver(self.__URI, auth=self.__AUTH) as driver:
+        with  GraphDatabase.driver(URI, auth=AUTH) as driver:
             for i in range(len(self.databases)):
                 if "RDF" in self.databases[i]: 
                     dbType = "RDF"
@@ -50,77 +46,81 @@ class performanceTest:
                 
                     # Execute second experiment
                     if "2" in self.file:
-                         session.execute_write(
+                        session.execute_write(
                             self.setup, 
                             dbType, 
                             self.databases[i]
                         )
-                        
-       
-    # Extract queries according their types and run the queries against each graph
+
     def setup(self, tx, dbType, dbName):
-       
-       # Query of type 1
-       measure_type1 = self.executeQueries(tx,self.queries[self.queries['Type']==1][dbType].to_list(), 1, dbName)
-       # Query of type 2
-       measure_type2 = self.executeQueries(tx, self.queries[self.queries['Type']==2][dbType].to_list(), 2,dbName)
-       # Query of type 3
-       measure_type3 = self.executeQueries(tx, self.queries[self.queries['Type']==3][dbType].to_list(), 3, dbName)
+        measures = self.executeQueries(tx, self.queries[dbType], dbName)
+        self.graphs_metrics[dbName]= measures.T
 
-       # Safe results of each graph in this dict 
-       self.graphs_measures[dbName]=pd.concat([measure_type1.T, measure_type2.T, measure_type3.T])
-      
     
-      
-    # Execute the queries (of a specifc type) and for a certain number of iterations
-    def executeQueries(self, tx, set, typeNumber, dbName): 
-        measuresType = pd.DataFrame({
-                            f"Type{typeNumber}_query1":{"Type":None, "query":None, "Total Execution Time":None, "Average Response Time":None, "Memory Usage":None}, 
-                            f"Type{typeNumber}_query2":{"Type":None, "query":None, "Total Execution Time":None, "Average Response Time":None, "Memory Usage":None},
-                            f"Type{typeNumber}_query3":{"Type":None, "query":None, "Total Execution Time":None, "Average Response Time":None, "Memory Usage":None},
-                            f"Type{typeNumber}_query4":{"Type":None, "query":None, "Total Execution Time":None, "Average Response Time":None, "Memory Usage":None}
-                        })
+    def executeQueries(self, tx, set, dbName):
+            
+            if "1" in self.file:
+                measuresType = pd.DataFrame({
+                    "Query1":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query2":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None}, 
+                    "Query3":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query4":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query5":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query6":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None}, 
+                    "Query7":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query8":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query9":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query10":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None}, 
+                    "Query11":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None},
+                    "Query12":{"Query":None, "Minimum Execution Time (µs)": None, "Maximum Execution Time (µs)": None, "Average Response Time (µs)": None}
+                })
+                execution_times={"query1":[], "query2":[], "query3":[], "query4":[], "query5":[], "query6":[], "query7":[], "query8":[], "query9":[], "query10":[], "query11":[], "query12":[]}
+            
+            else:
+                measuresType = pd.DataFrame({
+                    "Query1":{"Query":None, "Minimum Execution Time (µs)":None, "Maximum Execution Time (µs)":None, "Average Response Time (µs)":None}, 
+                    "Query2":{"Query":None, "Minimum Execution Time (µs)":None, "Maximum Execution Time (µs)":None, "Average Response Time (µs)":None},
+                    "Query3":{"Query":None, "Minimum Execution Time (µs)":None, "Maximum Execution Time (µs)":None, "Average Response Time (µs)":None}, 
+                    "Query4":{"Query":None, "Minimum Execution Time (µs)":None, "Maximum Execution Time (µs)":None, "Average Response Time (µs)":None},
+                    "Query5":{"Query":None, "Minimum Execution Time (µs)":None, "Maximum Execution Time (µs)":None, "Average Response Time (µs)":None}, 
+                    "Query6":{"Query":None, "Minimum Execution Time (µs)":None, "Maximum Execution Time (µs)":None, "Average Response Time (µs)":None},
+                    
+                })
+                execution_times={"query1":[], "query2":[], "query3":[], "query4":[], "query5":[], "query6":[]}
 
-        
-        for i , query in enumerate(set):
 
-            execution_times = [] # For saving the execution time in each iteration
-            memory_usages = []
+            for j in range(self.numberOfIteration):
+                with tqdm(total=len(set), desc=f"Graph: {dbName},  Iteration: {j+1}", unit="query") as pbar:
+                    for numberQuery, query in enumerate(set): 
 
-            with tqdm(total=self.numberOfIteration, desc=f"Graph: {dbName}, Type: {typeNumber},  Query: {i+1}", unit="query") as pbar:
-                for _ in range(self.numberOfIteration):
+                        time_before = time.perf_counter_ns() 
+                        results = tx.run(query)
+                        time_after = time.perf_counter_ns() 
 
-                    memory_before, start_time = self.measure_usage()
-                    tx.run(query)
-                    memory_after, end_time = self.measure_usage()
+                        results.consume()
+                        ex_time = (time_after-time_before)/1e3
 
-                    key = f"Type{typeNumber}_query{i+1}"
-                    execution_times.append( (end_time-start_time) *1e6) # execution time in microseconds
-                    memory_usages.append(memory_after-memory_before) # in bytes 
-                    pbar.update()
-        
+                        execution_times[f"query{numberQuery+1}"].append(ex_time)
+                        pbar.update()
 
-            measuresType[key]["Type"] = typeNumber
-            measuresType[key]["query"] = f"query_{i+1}"
-            measuresType[key]["Average Response Time"] = np.mean(execution_times)
-            measuresType[key]["Total Execution Time"] = np.median(execution_times)
-            measuresType[key]["Memory Usage"] = np.mean(memory_usages)
 
-        print(measuresType)
-        return measuresType
+            for i, _ in enumerate(set):
+                measuresType[f"Query{i+1}"]["Query"] = f"{i+1}"
+                measuresType[f"Query{i+1}"]["Average Response Time (µs)"] = np.mean(execution_times[f"query{i+1}"])
+                measuresType[f"Query{i+1}"]["Maximum Execution Time (µs)"] = max(execution_times[f"query{i+1}"])
+                measuresType[f"Query{i+1}"]["Minimum Execution Time (µs)"] = min(execution_times[f"query{i+1}"])
+
+            return measuresType
+    
 
     # Get the queries form the file
     def getQueriesFromFile(self,file):
         return pd.read_excel(file)
     
     # Create a cvs file with the results for each graph
-    def convertResults(self):
+    def convertResults(self, file):
         for db in self.databases:
-            df = self.graphs_measures[db]
-            df.to_csv(f"./experiment_1/results/{db}_measures", index=False)
+            df = self.graphs_metrics[db]
+            df.to_csv(f"{file}/{db}_measures", index=False)
 
-    def measure_usage(self):
-        p = psutil.Process(os.getpid())
-        return p.memory_full_info().rss ,timer()
-        
-
+    
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from experiments import performanceTest
 
 if __name__ == "__main__":
-    # first Experiment 
-    e1 = performanceTest(file ="./experiment_1/queries.xlsx", numberOfIteration=1001)
-    e1.executeExperiment()
-    e1.convertResults()
+    #first Experiment 
+    #e1 = performanceTest(file ="./experiment_1/queries.xlsx", numberOfIteration=1000)
+    #e1.executeExperiment()
+    #e1.convertResults("./experiment_1/results")
     
-    # second Experiment 
-  #  e2 = performanceTest("./Experiment_2/queries.xlsx", 11)
+    #second Experiment 
+    e2 = performanceTest("./experiment_2/queries.xlsx", 1000)
+    e2.executeExperiment()
+    e2.convertResults("./experiment_2/results")
