9024c28ae9854881362f001ef816a860684b80db
Nadine98 <Nadine.Kais@ruhr-uni-bochum.de>
2024-02-10 02:23:35 +0100

    [Experiment] changes

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from experiments import performanceTest
 
 if __name__ == "__main__":
     # first Experiment 
-    e1 = performanceTest.executeExperiment("./Experiment_1/queries.xlsx", 1000)
+    e1 = performanceTest(file ="./experiment_1/queries.xlsx", numberOfIteration=1001)
+    e1.executeExperiment()
+    e1.convertResults()
+    
     # second Experiment 
-    e2 = performanceTest.executeExperiment("./Experiment_2/queries.xlsx", 1000)
+  #  e2 = performanceTest("./Experiment_2/queries.xlsx", 11)
